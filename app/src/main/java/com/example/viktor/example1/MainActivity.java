ca66e5785b961404f4ced0e44774cb95ac15bd7b
Viktor <viktorvavrik@seznam.cz>
2019-01-02 00:39:57 +0100

    example3

--- app/src/main/java/com/example/viktor/example1/MainActivity.java
+++ app/src/main/java/com/example/viktor/example1/MainActivity.java
@@ -5,10 +5,13 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    int alfa;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        alfa = 0;
     }
 
     protected void onPause(Bundle savedInstanceState) {
